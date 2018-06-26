aac36ed65d1dab15a09d0bddd82667b12dbe644e
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-06-26 01:06:09 -0400

    Don't use chroma before script loads

--- index.user.js
+++ index.user.js
@@ -13,10 +13,10 @@
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/chroma-js/1.3.7/chroma.min.js";
     document.head.appendChild(script);
 
-    const scale = chroma.scale(["red", "orange", "yellow", "lime", "cyan", "mediumpurple", "red"]).colors(1000);
-    let index = 0;
-
     src.addEventListener("load", () => {
+        const scale = chroma.scale(["red", "orange", "yellow", "lime", "cyan", "mediumpurple", "red"]).colors(1000);
+        let index = 0;
+
         setInterval(() => {
             index += 1;
             if (index > scale.length) {
